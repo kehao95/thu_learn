eb27f48027efa9094bb79a8903b893d930c63fb8
Ke Hao <kehao95@gmail.com>
2015-08-03 16:17:32 +0800

    Edit elements structure

--- test.py
+++ test.py
@@ -1,20 +1,21 @@
 __author__ = 'kehao'
-from web_learning import *
-from web_learning import Semester
-from web_learning import Course
-from web_learning import Work
+from thu_learn import *
+from thu_learn import Semester
+from thu_learn import Course
+from thu_learn import Work
 
 
 def main():
     print('Crawler v0.1')
     try:
-        semester = Semester()
+        semester = Semester(current=False)
     except ValueError:
         print('failed to get Semester')
-    for course in semester.courses():
+    for course in semester.courses:
         print(course.name, course.id, course.url)
-        for work in course.works():
+        for work in course.works:
             print(work.title)
+
 
 
 if __name__ == '__main__':
